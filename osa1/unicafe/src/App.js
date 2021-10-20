8177bcccd4688d12d751daccdef570056d6ad7f1
Emma Valtonen <emma.valtonen@helsinki.fi>
2021-10-20 13:25:41 +0300

    Create feedback app, exercise 1.6

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const Button = ({ handleClick, text }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
 }
 
-export default App;
+const Stats = ({ text, stats }) => {
+  return (
+    <div>{text} {stats}</div>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <Button handleClick={handleGoodClick} text='good' />
+      <Button handleClick={handNeutralClick} text='neutral' />
+      <Button handleClick={handleBadClick} text='bad' />
+      <h1>Statistics</h1>
+      <Stats text='good' stats={good}/>
+      <Stats text='neutral' stats={neutral}/>
+      <Stats text='bad' stats={bad}/>
+    </div>
+  )
+}
+
+export default App
