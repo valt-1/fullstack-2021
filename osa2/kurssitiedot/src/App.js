29d1ca3639482c93f182cb7bc25a566e25359192
Emma Valtonen <emma.valtonen@helsinki.fi>
2021-10-27 23:20:29 +0300

    Add total of exercises, exercise 2.2 & 2.3

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -25,11 +25,19 @@ const Content = ({ course }) => {
   )
 }
 
+const Total = ({ course }) => {
+  const total = course.parts.reduce((total, part) => total + part.exercises, 0)
+  return (
+    <div>Total of {total} exercises</div>
+  )
+}
+
 const Course = ({ course }) => {
   return (
     <div>
       <Header course={course} />
       <Content course={course} />
+      <Total course={course}/>
     </div>
   )
 }
