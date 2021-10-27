ec5f24f532a76022385946ea27c7b04558a725de
Emma Valtonen <emma.valtonen@helsinki.fi>
2021-10-27 23:36:34 +0300

    Refactor, exercise 2.5

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,46 +1,5 @@
 import React from 'react'
-
-const Header = ({ course }) => {
-  return (
-    <h2>{course.name}</h2>
-  )
-}
-
-const Part = ({ part }) => {
-  return (
-    <li>{part.name} {part.exercises}</li>
-  )
-}
-
-const Content = ({ course }) => {
-  const parts = course.parts
-  return (
-    <div>
-      <ul>
-        {parts.map(part =>
-          <Part key={part.id} part={part}/>
-        )}
-      </ul>
-    </div>
-  )
-}
-
-const Total = ({ course }) => {
-  const total = course.parts.reduce((total, part) => total + part.exercises, 0)
-  return (
-    <div>Total of {total} exercises</div>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course}/>
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
