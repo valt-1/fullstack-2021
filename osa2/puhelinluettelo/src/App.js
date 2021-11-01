f910040d3284f0fa64d9ecff012d2db072287d7d
Emma Valtonen <emma.valtonen@helsinki.fi>
2021-11-01 17:29:51 +0200

    Add possibility to add names, exercise 2.6

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,24 +1,50 @@
 import React, { useState } from 'react'
 
+const Person = ({ person }) => {
+  return (
+    <li>{person.name}</li>
+  )
+}
+
 const App = () => {
   const [ persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handlePersonChange = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: 
+          <input 
+            value={newName}
+            onChange={handlePersonChange}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map(person =>
+          <Person key={person.name} person={person} />)}
+      </ul>
     </div>
   )
 
