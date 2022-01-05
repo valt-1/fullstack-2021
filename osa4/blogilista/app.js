2f90f1dc9722044f74f6c917d05645169ac9105d
Emma Valtonen <emma.valtonen@helsinki.fi>
2022-01-05 13:30:33 +0200

    Refactor, add logger & db error handling, exercise 4.2

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -3,9 +3,12 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
 
 mongoose.connect(config.MONGODB_URI)
+  .then(() => logger.info('connected to MongoDB'))
+  .catch(error => logger.error('error connecting to MongoDB: ', error.message))
 
 app.use(cors())
 app.use(express.json())
