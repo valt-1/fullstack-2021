08fe0a740ffb133a59c4fc2476d36cef03caca6a
Emma Valtonen <emma.valtonen@helsinki.fi>
2022-01-05 15:00:55 +0200

    Add function favoriteBlog & tests, exercise 4.5

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -6,7 +6,19 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return undefined
+
+  let favorite = blogs[0]
+  for (let i = 0; i < blogs.length; i++) {
+    if (blogs[i].likes > favorite.likes) favorite = blogs[i]
+  }
+
+  return favorite
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
