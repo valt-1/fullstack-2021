f672b33bb9eb33e0905ea7e9a1f650eacced9061
Emma Valtonen <emma.valtonen@helsinki.fi>
2022-02-14 14:04:31 +0200

    Add login functionality, exercise 5.1

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState (null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,9 +15,51 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log('wrong credentials')
+    }
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>log in</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+            <input
+            type='text'
+            value={username}
+            name='username'
+            onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password
+            <input
+            type='password'
+            value={password}
+            name='password'
+            onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type='submit'>login</button>
+        </form>
+      </div>
+    )
+  }
+
   return (
     <div>
       <h2>blogs</h2>
+      <p>{user.name} logged in</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
