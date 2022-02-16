e3382589634fdfcbec5c2c6365aacd654497394f
Emma Valtonen <emma.valtonen@helsinki.fi>
2022-02-16 13:58:42 +0200

    Make a single blog's data togglable, exercise 5.7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,34 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    padding: 2,
+    backgroundColor: '#fce3f9',
+    margin: 3
+  }
+
+  const [showAll, setShowAll] = useState(false)
+
+  const toggleShowAll = () => {
+    setShowAll(!showAll)
+  }
+
+  if (!showAll) {
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author} <button onClick={toggleShowAll}>view</button>
+      </div>  
+    )
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author} <button onClick={toggleShowAll}>hide</button><br/>
+      {blog.url}<br/>
+      likes: {blog.likes} <button>like</button><br/>
+      {blog.user.name}
+    </div>  
+  )
+}
 
 export default Blog
