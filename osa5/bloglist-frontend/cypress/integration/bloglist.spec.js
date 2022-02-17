ad8887b53df36b920bcc1fd180e77667cdcd5fea
Emma Valtonen <emma.valtonen@helsinki.fi>
2022-02-17 15:31:13 +0200

    Add login tests, exercise 5.18

--- osa5/bloglist-frontend/cypress/integration/bloglist.spec.js
+++ osa5/bloglist-frontend/cypress/integration/bloglist.spec.js
@@ -2,6 +2,11 @@ describe ('Blog app', function() {
 
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.request('POST', 'http://localhost:3003/api/users', {
+      name: 'Rata-Marjatta',
+      username: 'ratis',
+      password: 'password123'
+    }, )
     cy.visit('http://localhost:3000')
   })
 
@@ -10,6 +15,26 @@ describe ('Blog app', function() {
     cy.get('#username')
     cy.get('#password')
     cy.get('#loginButton')
+  })
+
+  describe('Login', function() {
+
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('ratis')
+      cy.get('#password').type('password123')
+      cy.get('#loginButton').click()
+
+      cy.contains('Rata-Marjatta logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('ratis')
+      cy.get('#password').type('topsecret')
+      cy.get('#loginButton').click()
+
+      cy.contains('login failed')
+    })
+
   })
 
 })
