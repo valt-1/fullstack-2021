e987050a027b842ce3767e3986257add9086eb97
Emma Valtonen <emma.valtonen@helsinki.fi>
2022-02-18 15:47:07 +0200

    Sort anecdotes by number of votes, exercise 6.5, 6.6 also done

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -29,7 +29,8 @@ const reducer = (state = initialState, action) => {
         ...anecdote,
         votes: anecdote.votes + 1
       }
-      return state.map(a => a.id !== id ? a : updatedAnecdote)
+      const newState = state.map(a => a.id !== id ? a : updatedAnecdote)
+      return newState.sort((a1, a2) => a2.votes - a1.votes)
     }
 
     case 'NEW_ANECDOTE': {
